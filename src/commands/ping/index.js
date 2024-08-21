a2c0385745429c0ae30a3f73aa6e256c9d5cf3f0
tommy5837947 <tommy70758465@gmail.com>
2024-08-21 15:22:57 +0800

    8/21 建立斜線指令(尚未註冊)

---
+++ src/commands/ping/index.js
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder } from 'discord.js'
+
+export const command = new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('ping command')
+
+
+export const action = async (ctx) => {
+    ctx.reply('pong！')
+}
+
+//export default{
+//  command: command = new SlashCommandBuilder()
+//      .setName('ping')
+//      .setDescription('ping command')
+//
+//
+//  action: action = async (ctx) => {
+//      ctx.reply('pong！')
+//  }
+//}
